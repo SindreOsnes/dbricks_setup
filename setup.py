9fa18b430cf59617f5f56f45d97ed9b3d9d01910
Sindre Osnes <sindreosnes.git@gmail.com>
2021-08-31 02:43:45 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dbricks_setup',
     description='Python module for generating a wrapper around the databricks-cli for setting up clusters and secret scopes.',
     license="MIT",
-    url="https://github.com/Unicus-International/databricks_setup",
+    url="https://github.com/SindreOsnes/dbricks_setup",
     author='Sindre Osnes',
     author_email='sindreosnes.git@gmail.com',
     version='0.0.1',
